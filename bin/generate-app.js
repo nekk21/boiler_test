14fae30a760930281a92ea7ef2a0b104c784a155
Maksym Sid <maxim.sidorov@tech-stack.io>
2022-08-09 12:25:05 +0300

    test

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -30,7 +30,7 @@ if (process.argv.length < 3) {
 const ownPath = process.cwd();
 const folderName = process.argv[2];
 const appPath = path.join(ownPath, folderName);
-const repo = 'https://github.com/Leopold-V/react-parcel-app.git';
+const repo = 'https://github.com/nekk21/test.git';
 
 try {
   fs.mkdirSync(appPath);
